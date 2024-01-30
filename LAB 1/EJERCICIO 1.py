b72d360566a652d580e49eb13f41448b74450b99
Daniel Ruiz <edgar.ruiz@mail.escuelaing.edu.co>
2024-01-29 21:48:33 -0500

    LAB1

---
+++ LAB 1/EJERCICIO 1.py
@@ -0,0 +1,35 @@
+#PROGRAMA 1 NÚMERO PALINDROMO
+
+print("Por favor escriba una palabra o frase, el programa verificará cada palabra escrita (pueden ser números) y dirá si es palíndromo o no.")
+texto=input("Ingresar texto aquí: ")
+cantidad=0
+palabra=""
+#print(len(texto))
+for c in texto:
+    #print (c)
+    palabra=palabra+c
+    if c==" ":
+        cantidad=cantidad+1 #Cuenta el número de palabras
+        palabra=palabra[:-1]
+        #print(palabra)
+        palabrainvertida=palabra[::-1]
+        #print(palabrainvertida)
+        if palabra==palabrainvertida:
+            print('La palabra', palabra , 'es palindroma')
+            palabra="";
+            palabrainvertida="";
+        else:
+            print('La palabra', palabra , 'no es palindroma')
+            palabra="";
+palabrainvertida=palabra[::-1]
+cantidad=cantidad+1
+if palabra==palabrainvertida:
+    print('La palabra', palabra , 'es palindroma')
+    palabra="";
+    palabrainvertida="";
+else:
+    print('La palabra', palabra , 'no es palindroma')
+    palabra="";
+print('Se analizaron', cantidad ,'palabras');
+
+        
---
+++ LAB 1/EJERCICIO 4.py
@@ -0,0 +1,12 @@
+#PROGRAMA 2 PROBLEMA DE SUBCADENAS DISTINTAS
+
+texto1=input("Ingrese la cadena original: ")
+#print (texto1)
+textorotado=texto1
+cantidad=0;
+for i in range (0,len(texto1)):
+    textorotado= textorotado[1:]+textorotado[:1]
+    print(textorotado)
+    cantidad=cantidad +1
+
+print ("La cantidad de cadenas posibles es: ",cantidad)
